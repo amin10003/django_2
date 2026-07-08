7bda5935547d2ef2742de5c71a84dc7daea281de
amin10003 <maxamedxasancali10003@gmail.com>
2026-07-08 11:52:20 +0300

    add templates and url in coloriProject

--- colorieProject/views.py
+++ colorieProject/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def profile(request):
+    return render(request, "profile.html")
+
+def dashboard(request):
+    content = {"content": "Hello Guest"}
+    return render(request, "dashboard.html", content)
+
+def login(request):
+    return render(request, "login.html")
