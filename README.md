b7008d28299f5b1f7686b5e9f6b9dcdd837ff6a2
amin10003 <maxamedxasancali10003@gmail.com>
2026-07-11 15:22:03 +0300

    add Auth and blog class to contrlo databases from django

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # django_2
+create a .env file and add the following content as required
+
+SECRET_KEY= "paste you secret from the django project settings.py file"
+DATABASE_NAME="name of your database"
+DATABASE_USER="database user name"
+DATABASE_PASSWORD="your database password"
+DATABASE_HOST="your database host"
+DATABASE_PORT="your database port"
+DEBUG=True
+CLOUDINARY_CLOUD_NAME="your cloudinary cloud name"
+CLOUDINARY_API_KEY="your created project api key"
+CLOUDINARY_API_SECRET="your created project secret key"
--- colorieProject/models.py
+++ colorieProject/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+#   """
+#   Django model is a python class and it becomes a table in your database.
+#    Evertimme you update your model , make sure you migrate the changes
+#   """
+ 
+ 
+class Author(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField()
+    
+    def __str__(self):
+        return f"{self.first_name}"
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    published_date = models.DateTimeField( blank=True)
+    created_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.title}"
